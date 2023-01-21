972d783a34daa88d4329d6aaf86c628f46963f19
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-21 12:19:49 +0100

    store example url in variable

--- components/CardList/index.js
+++ components/CardList/index.js
@@ -1,17 +1,9 @@
 import Card from "../Card";
-import useSWR from "swr";
 
-export default function CardList() {
-  const { data, error, isLoading } = useSWR(
-    "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PLiiteex9CDbg-EAskekSQHbfj1j8WaaxM&key=AIzaSyBMSlB4mSGgur42EkHOKKMmTPtqOTSkRI4&part=snippet&maxResults=112"
-  );
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
-
+export default function CardList({ tutorials }) {
   return (
     <ul>
-      {data.items.map((tutorial) => {
+      {tutorials.map((tutorial) => {
         return (
           <li key={tutorial.id}>
             <Card content={tutorial.snippet} />
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import CardList from "@/components/CardList";
 import Header from "@/components/Header";
+import useSWR from "swr";
+
+const url =
+  "https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PLiiteex9CDbg-EAskekSQHbfj1j8WaaxM&key=AIzaSyBMSlB4mSGgur42EkHOKKMmTPtqOTSkRI4&part=snippet&maxResults=112";
 
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(url);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <>
       <Header />
-      <CardList />
+      <CardList tutorials={data.items} />
     </>
   );
 }
