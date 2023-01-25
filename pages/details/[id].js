10573f23e0f01bd4d9084e3bf4e2386f8899caab
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-25 12:48:40 +0100

    add bookmarkpage

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -20,6 +20,7 @@ export default function DetailsPage() {
       })
     );
   }
+
   const currentTutorial = list.find((tutorial) => {
     return tutorial.id === id;
   });
--- pages/repertoire/index.js
+++ pages/repertoire/index.js
@@ -1,24 +1,15 @@
 import CardList from "@/components/CardList";
+import { beginnerList } from "@/mockData/globalBeginner52";
+import { useAtom } from "jotai";
 
 export default function Repertoire() {
-  // function toggleBookmark(id) {
-  //   const BookmarkedArray = tutorials.map((tutorial) => {
-  //     if (tutorial.id === id) {
-  //       return {
-  //         ...tutorial,
-  //         isBookmarked: !tutorial.isBookmarked,
-  //       };
-  //     } else {
-  //       return tutorial;
-  //     }
-  //   });
-  //   setTutorials(newTutorialArray);
-  // }
+  const [list] = useAtom(beginnerList);
+  const filteredList = list.filter((content) => content.isLiked === true);
 
   return (
     <section>
       <h2>your Repertoire</h2>
-      <CardList />
+      <CardList tutorials={filteredList} />
     </section>
   );
 }
