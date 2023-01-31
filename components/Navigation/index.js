0c3b02d3fe6ea9cec5bd9600a664339c06a72b6a
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-31 11:59:47 +0100

    fix nav

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { StyledNavigation } from "./Navigation.styled";
+import { StyledNavigation, StyledNavLink } from "./Navigation.styled";
 import SVGIcon from "../SVGIcon";
 import { useRouter } from "next/router";
 
@@ -12,34 +12,34 @@ export default function Navigation() {
         <Link href="/tutorials"></Link>
       ) : (
         <StyledNavigation>
-          <Link href="/tutorials">
+          <StyledNavLink href="/tutorials">
             <SVGIcon
               variant={pathname === "/tutorials" ? "list" : "listOutline"}
               width="40px"
             />
             Tutorials
-          </Link>
-          <Link href="/search">
+          </StyledNavLink>
+          <StyledNavLink href="/search">
             <SVGIcon
               variant={pathname === "/search" ? "search" : "searchOutline"}
               width="40px"
             />
             Search
-          </Link>
-          <Link href="/repertoire">
+          </StyledNavLink>
+          <StyledNavLink href="/repertoire">
             <SVGIcon
               variant={pathname === "/repertoire" ? "heart" : "heartOutline"}
               width="40px"
             />
             Repertoire
-          </Link>
-          <Link href="/profil">
+          </StyledNavLink>
+          <StyledNavLink href="/profil">
             <SVGIcon
               variant={pathname === "/profil" ? "user" : "userOutline"}
               width="40px"
             />
             Profil
-          </Link>
+          </StyledNavLink>
         </StyledNavigation>
       )}
     </>
