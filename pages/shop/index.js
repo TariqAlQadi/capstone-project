b139ae5a1a80bf5e0956990aa240bdd983b354b7
TariqAlQadi <tariqalqadi@gmx.de>
2023-01-31 13:13:06 +0100

    finish shop list

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,12 +1,69 @@
+import Link from "next/link";
+
 export default function Shop() {
   return (
     <section>
       <h2>Online Shops:</h2>
       <ul>
-        <li>Murphys Magic</li>
-        <li>Ellusionist</li>
-        <li>Theory11</li>
-        <li>MagicShop</li>
+        <li>
+          <Link href="https://www.murphysmagic.com">Murphys Magic</Link>
+        </li>
+        <li>
+          <Link href="https://ellusionist.com">Ellusionist</Link>
+        </li>
+        <li>
+          <Link href="https://www.theory11.com">Theory11</Link>
+        </li>
+        <li>
+          <Link href="https://www.magicshop.ch" i>
+            MagicShop
+          </Link>
+        </li>
+        <li>
+          <Link href="https://pinkybreak.com" i>
+            pinkybreak
+          </Link>
+        </li>
+        <li>
+          <Link href="https://anyoneworldwide.com" i>
+            Anyone
+          </Link>
+        </li>
+        <li>
+          <Link href="https://plainsightmagic.com" i>
+            Plainsight
+          </Link>
+        </li>
+        <li>
+          <Link href="https://hhpresents.com" i>
+            Henry Harrius
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.vanishingincmagic.com" i>
+            Vanishing Inc.
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.penguinmagic.com/" i>
+            Penguin
+          </Link>
+        </li>
+        <li>
+          <Link href="https://stemaro-magic.de" i>
+            Stemaro
+          </Link>
+        </li>
+        <li>
+          <Link href="https://promystic.com/" i>
+            ProMystic
+          </Link>
+        </li>
+        <li>
+          <Link href="https://askalexander.org" i>
+            Ask Alexander
+          </Link>
+        </li>
       </ul>
     </section>
   );
