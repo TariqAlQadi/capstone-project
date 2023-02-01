27f2ba1daaed10c29da4aa377ac7bdb23586c53f
TariqAlQadi <tariqalqadi@gmx.de>
2023-02-01 11:02:42 +0100

    last change for refactor part I

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,58 +1,37 @@
 import Link from "next/link";
 
+const onlineShops = [
+  { name: "Murphsy Magic", href: "https://www.murphysmagic.com" },
+  { name: "Ellusionist", href: "https://ellusionist.com" },
+  { name: "Theory11", href: "https://www.theory11.com" },
+  { name: "MagicShop", href: "https://www.magicshop.ch" },
+  { name: "PinkyBreak", href: "https://pinkybreak.com" },
+  { name: "Anyone", href: "https://anyoneworldwide.com" },
+  { name: "Plainsight", href: "https://plainsightmagic.com" },
+  { name: "Vanishing Inc.", href: "https://www.vanishingincmagic.com" },
+  { name: "Penguin", href: "https://www.penguinmagic.com" },
+  { name: "Stemaro", href: "https://stemaro-magic.de" },
+  { name: "ProMystic", href: "https://promystic.com" },
+];
+
 export default function Shop() {
   return (
     <section>
       <h2>Online Shops:</h2>
       <ul>
-        <li>
-          <Link href="https://www.murphysmagic.com">Murphys Magic</Link>
-        </li>
-        <li>
-          <Link href="https://ellusionist.com">Ellusionist</Link>
-        </li>
-        <li>
-          <Link href="https://www.theory11.com">Theory11</Link>
-        </li>
-        <li>
-          <Link href="https://www.magicshop.ch">MagicShop</Link>
-        </li>
-        <li>
-          <Link href="https://pinkybreak.com">pinkybreak</Link>
-        </li>
-        <li>
-          <Link href="https://anyoneworldwide.com">Anyone</Link>
-        </li>
-        <li>
-          <Link href="https://plainsightmagic.com">Plainsight</Link>
-        </li>
-        <li>
-          <Link href="https://hhpresents.com">Henry Harrius</Link>
-        </li>
-        <li>
-          <Link href="https://www.vanishingincmagic.com" i>
-            Vanishing Inc.
-          </Link>
-        </li>
-        <li>
-          <Link href="https://www.penguinmagic.com/" i>
-            Penguin
-          </Link>
-        </li>
-        <li>
-          <Link href="https://stemaro-magic.de" i>
-            Stemaro
-          </Link>
-        </li>
-        <li>
-          <Link href="https://promystic.com/" i>
-            ProMystic
-          </Link>
-        </li>
+        {onlineShops.map((shop) => {
+          return (
+            <li key={shop.href}>
+              <Link href={shop.href}>{shop.name}</Link>
+            </li>
+          );
+        })}
       </ul>
       <h2>Best Search Engine:</h2>
-      <Link href="https://askalexander.org" i>
-        Ask Alexander
+      <Link href="https://askalexander.org">Ask Alexander</Link>
+      <h2>Forum:</h2>
+      <Link href="https://www.themagiccafe.com/forums/index.php">
+        The Magic Café
       </Link>
     </section>
   );
