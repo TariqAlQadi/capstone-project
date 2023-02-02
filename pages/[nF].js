be0ac5b3d1e647adb5123f6ad2f564a6aa999af9
TariqAlQadi <tariqalqadi@gmx.de>
2023-02-02 16:29:22 +0100

    small bug ... need to fx favi icon

--- pages/[nF].js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useRouter } from "next/router";
-
-export default function NotFound() {
-  const router = useRouter();
-  const { nF } = router.query;
-  return <div>404 Not Found</div>;
-}
