67f1a5b36a65ddaa0ba173b015d7330df29b1ce6
TariqAlQadi <tariqalqadi@gmx.de>
2023-02-10 15:45:30 +0100

    edit user api

--- pages/api/tutorials/index.js
+++ pages/api/tutorials/index.js
@@ -1,11 +1,8 @@
 import dbConnect from "@/db/connect";
 import Tutorial from "@/db/models/Tutorial";
-import { getToken } from "next-auth/jwt";
 
 export default async function handler(request, response) {
   await dbConnect();
-
-  const token = await getToken({ req: request });
 
   if (request.method === "GET") {
     const tutorials = await Tutorial.find();
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,11 +1,32 @@
 import dbConnect from "@/db/connect";
 import User from "@/db/models/User";
+import { getToken } from "next-auth/jwt";
 
 export default async function handler(request, response) {
-  await dbConnect();
+  const conn = await dbConnect();
 
-  if (request.method === "GET") {
-    const users = await User.find();
-    return response.status(200).json(users);
-  }
+  const session = await conn.startSession();
+
+  await session.withTransaction(async () => {
+    const token = await getToken({ request });
+
+    if (token && req.method === "GET") {
+      const existingUser = await User.findOne({ sub: token.sub });
+
+      if (!existingUser) {
+        const newUser = new User({
+          sub: token.sub,
+        });
+
+        await newUser.save();
+
+        return response
+          .status(201)
+          .json(newUser, { status: "Created a new user" });
+      }
+      return response.status(200).json(existingUser);
+    }
+  });
+
+  session.endSession();
 }
