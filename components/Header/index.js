2f1f59118bcf30ec6adf6c35c4eddd3140c8551e
TariqAlQadi <tariqalqadi@gmx.de>
2023-02-10 13:33:56 +0100

    delete unneccessary code

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,23 +2,22 @@ import { StyledHeader } from "./Header.styled";
 import { useRouter } from "next/router";
 import SVGIcon from "../SVGIcon";
 import { StyledLogOutButton } from "./Header.styled";
+import { signOut } from "next-auth/react";
 
 export default function Header() {
   const { pathname } = useRouter();
-  const router = useRouter();
-
-  //log out function
-  function handleLogOut() {
-    localStorage.clear();
-    router.push("/");
-  }
 
   return (
     <>
       {pathname !== "/" && (
         <StyledHeader>
           <h1>NeTrix</h1>
-          <StyledLogOutButton onClick={handleLogOut} aria-label="log-out">
+          <StyledLogOutButton
+            onClick={() => {
+              signOut();
+            }}
+            aria-label="log-out"
+          >
             <SVGIcon variant="logOut" width="20px" color="black" />
           </StyledLogOutButton>
         </StyledHeader>
