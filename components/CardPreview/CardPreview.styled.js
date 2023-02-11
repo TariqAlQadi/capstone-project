c58565e764b571709f489ab0f81b0af8d15730f6
TariqAlQadi <tariqalqadi@gmx.de>
2023-02-11 23:05:46 +0100

    added cardpreview styling

--- components/CardPreview/CardPreview.styled.js
+++ components/CardPreview/CardPreview.styled.js
@@ -8,3 +8,15 @@ export const StyledSubtitle = styled.h4`
 export const StyledImage = styled(Image)`
   clip-path: polygon(0 13%, 100% 13%, 100% 87%, 0 87%);
 `;
+
+export const StyledTopLine = styled.hr`
+  color: var(--passive-color);
+  position: relative;
+  top: 18px;
+`;
+
+export const StyledBottomLine = styled.hr`
+  color: var(--passive-color);
+  position: relative;
+  bottom: 20px;
+`;
--- components/CardPreview/index.js
+++ components/CardPreview/index.js
@@ -1,17 +1,24 @@
 import { StyledLink } from "../StyledLink/Link.styled";
-import { StyledImage, StyledSubtitle } from "./CardPreview.styled";
+import {
+  StyledImage,
+  StyledTopLine,
+  StyledSubtitle,
+  StyledBottomLine,
+} from "./CardPreview.styled";
 
 export default function CardPreview({ content, _id }) {
   return (
     <StyledLink href={`/details/${_id}`}>
       <h3>{content?.title}</h3>
       <StyledSubtitle>by {content?.videoOwnerChannelTitle}</StyledSubtitle>
+      <StyledTopLine />
       <StyledImage
         src={content?.thumbnails.high.url}
         alt={content?.title}
         width={300}
         height={300}
       />
+      <StyledBottomLine />
     </StyledLink>
   );
 }
