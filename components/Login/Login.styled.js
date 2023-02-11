2e08729c3fa0acd4aa6d2a3732b400d3476aee02
TariqAlQadi <tariqalqadi@gmx.de>
2023-02-11 15:16:50 +0100

    styled login component

--- components/Login/Login.styled.js
+++ components/Login/Login.styled.js
@@ -10,3 +10,30 @@ export const StyledSection = styled.section`
   text-align: center;
   border: 1px solid black;
 `;
+
+export const ClubsWrapper = styled.div`
+  rotate: -45deg;
+  position: absolute;
+  left: 15px;
+  top: 15px;
+`;
+export const HeartsWrapper = styled.div`
+  rotate: 45deg;
+  position: absolute;
+  right: 15px;
+  top: 15px;
+`;
+
+export const SpadesWrapper = styled.div`
+  rotate: 135deg;
+  position: absolute;
+  top: 120px;
+  right: 15px;
+`;
+
+export const DiamondsWrapper = styled.div`
+  rotate: 45deg;
+  position: absolute;
+  left: 15px;
+  top: 120px;
+`;
--- components/Login/index.js
+++ components/Login/index.js
@@ -1,9 +1,29 @@
 import { signIn } from "next-auth/react";
-import { StyledLoginButton, StyledSection } from "./Login.styled";
+import {
+  StyledLoginButton,
+  StyledSection,
+  ClubsWrapper,
+  HeartsWrapper,
+  SpadesWrapper,
+  DiamondsWrapper,
+} from "./Login.styled";
+import SVGIcon from "../SVGIcon";
 
 export default function Login() {
   return (
     <StyledSection>
+      <ClubsWrapper>
+        <SVGIcon variant="clubs" width="20px" color="black" />
+      </ClubsWrapper>
+      <HeartsWrapper>
+        <SVGIcon variant="heart" width="20px" color="darkred" />
+      </HeartsWrapper>
+      <SpadesWrapper>
+        <SVGIcon variant="spades" width="20px" color="black" />
+      </SpadesWrapper>
+      <DiamondsWrapper>
+        <SVGIcon variant="diamonds" width="20px" color="darkred" />
+      </DiamondsWrapper>
       <h1>Welcome to Netrix</h1>
       <p>This is a Web Application to discover, learn and create Magic!</p>
       <StyledLoginButton
